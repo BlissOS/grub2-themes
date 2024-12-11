da9e718246b5b67ba6f9d50d71b575d7d0bffd07
shadichy <hotrobb360web@gmail.com>
2024-12-11 15:59:44 +0700

    Automated workflow

--- build-deb.sh
+++ build-deb.sh
@@ -3,9 +3,27 @@
 
 cd "$(dirname "$0")"
 
-current_ver=$(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}')
+pkgname=grub-theme-blissos
+pkgver=$(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}')
 
 # Create .orig tarball
-tar -cJf ../grub-theme-blissos_${current_ver}.orig.tar.xz .
+tar -cJf ../${pkgname}_${pkgver}.orig.tar.xz .
 
 dpkg-buildpackage -b --no-sign
+
+THEME_VARIANTS=('vimix' 'tela' 'stylish' 'whitesur')
+ICON_VARIANTS=('white' 'color' 'whitesur')
+SCREEN_VARIANTS=('1080p' '2k' '4k' 'ultrawide' 'ultrawide2k')
+
+# export metadata
+cat <<EOF >../metadata.yml
+Name: $pkgname
+Version: $pkgver
+Variants: $(for theme in "${THEME_VARIANTS[@]}"; do
+  for icon in "${ICON_VARIANTS[@]}"; do
+    for res in "${SCREEN_VARIANTS[@]}"; do
+      echo -n " ${theme}-${icon}-${res}"
+    done
+  done
+done)
+EOF
--- debian/grub-theme-blissos.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the grub-theme-blissos package.
-#
-0 4	* * *	root	[ -x /usr/bin/grub-theme-blissos_maintenance ] && /usr/bin/grub-theme-blissos_maintenance
