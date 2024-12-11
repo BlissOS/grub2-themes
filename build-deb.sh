8d66e2399f96fa556864abf4d1d9f7e6c6f9ad39
shadichy <hotrobb360web@gmail.com>
2024-12-11 17:10:24 +0700

    Update metadata

--- build-deb.sh
+++ build-deb.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# shellcheck disable=SC2086,2103,2164
+# shellcheck disable=2086,2103,2164,2317
 
 cd "$(dirname "$0")"
 
 pkgname=grub-theme-blissos
-pkgver=$(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}')
+_ver=$(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}-[0-9]+')
+pkgver=$(echo "${_ver}" | awk -F - '{print $1}')
+pkgrel=$(echo "${_ver}" | awk -F - '{print $2}')
+
+# avoid command failure
+exit_check() { [ "$1" = 0 ] || exit "$1"; }
+trap 'exit_check $?' EXIT
 
 # Create .orig tarball
 tar -cJf ../${pkgname}_${pkgver}.orig.tar.xz .
@@ -17,8 +23,8 @@ SCREEN_VARIANTS=('1080p' '2k' '4k' 'ultrawide' 'ultrawide2k')
 
 # export metadata
 cat <<EOF >../metadata.yml
-Name: $pkgname
-Version: $pkgver
+Name: ${pkgname}
+Version: ${pkgver}-${pkgrel}
 Variants: $(for theme in "${THEME_VARIANTS[@]}"; do
   for icon in "${ICON_VARIANTS[@]}"; do
     for res in "${SCREEN_VARIANTS[@]}"; do
